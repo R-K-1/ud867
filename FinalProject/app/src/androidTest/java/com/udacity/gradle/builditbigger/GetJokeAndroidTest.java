dd919e1bd3f3bbd4f8dc2597023933c20a99669b
R-K-1 <raymondkalonji@gmail.com>
2017-02-27 20:12:58 -0500

    Moving connected test to appropriate location.

---
+++ FinalProject/app/src/androidTest/java/com/udacity/gradle/builditbigger/GetJokeAndroidTest.java
@@ -0,0 +1,37 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import android.util.Pair;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.Assert.assertFalse;
+
+/**
+ * Created by rkalonji on 02/27/2017.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class GetJokeAndroidTest {
+
+    @Test
+    public void verifyAsyncTaskReturnJoke() {
+        EndpointsAsyncTask endpointsAsyncTask = new EndpointsAsyncTask();
+
+        Context context = InstrumentationRegistry.getTargetContext();
+
+/*        endpointsAsyncTask.execute(new Pair<Context, String>(context, "Manfred"));
+        do {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } while (endpointsAsyncTask.getHasExecuted() == false);
+
+        assertFalse(endpointsAsyncTask.getJoke() == "");*/
+    }
+}
